5546677c8a01934d21d8fdf29371f8ae4d3f6123
Marta Zaorska <marta.zaorska2@gmail.com>
2024-10-28 13:41:41 +0100

    clean up layout.tsx

--- next/src/app/layout.tsx
+++ next/src/app/layout.tsx
@@ -1,14 +1,9 @@
 import '@/global/global.scss';
 import { LOCALE } from '@/global/constants';
-import type { Metadata } from 'next';
 import Header from '@/components/global/Header';
 import Footer from '@/components/global/Footer';
 import SchemaOrganization from '@/global/Schema/Organization';
 import { SmoothScrollProvider } from '@/components/ui/SmoothScroll';
-
-export const metadata: Metadata = {
-  title: 'Foto Studio',
-};
 
 export default function RootLayout({
   children,
