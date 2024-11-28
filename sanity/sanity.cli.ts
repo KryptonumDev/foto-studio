6d0fdb2207b2e61ed731e8cec7e59bcad9f92557
OliwierSellig54 <oliwierandrzej.sellig@gmail.com>
2024-11-28 16:49:13 +0100

    change sanity source

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'l2bwizzm',
+    projectId: 'ltc5aw1t',
     dataset: 'production',
   },
   studioHost: 'adamchrapekfotostudio',
--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   name: 'default',
   title: 'foto-studio',
 
-  projectId: 'l2bwizzm',
+  projectId: 'ltc5aw1t',
   dataset: 'production',
 
   plugins: [
